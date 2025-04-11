99d0e7626fa5eff288c12e32f8542f07972013bf
cyeons <cyeons@gmail.com>
2025-04-11 09:35:22 +0900

    fix: process.env.PORT

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { isReliableResult } = require('./utils/validate');
 require('dotenv').config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
